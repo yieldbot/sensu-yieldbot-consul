694429d019aab3240a22dcfda19dff2849a71374
Matt Jones <mattjones@yieldbot.com>
2015-04-02 11:53:44 -0400

    move version file

--- lib/sensu-yieldbot-consul.rb
+++ lib/sensu-yieldbot-consul.rb
@@ -1,44 +1,14 @@
-require 'json'
 
-# encoding: utf-8
+require 'sensu-yieldbot-consul/version'
+
+# Load the defaults
+
 module SensuYieldbotConsul
-  # This defines the version of the gem
-  module Version
-    MAJOR = 0
-    MINOR = 0
-    PATCH = 4
+  class << self
+    attr_writer :ui
+  end
 
-    STRING = [MAJOR, MINOR, PATCH].compact.join('.')
-
-    NAME   = 'Sensu-Yieldbot-Consul'
-    BANNER = "#{NAME} v%s"
-
-    module_function
-
-    def version
-      format(BANNER, STRING)
-    end
-
-    def json_version
-      {
-        'version' => STRING
-      }.to_json
-    end
+  class << self
+    attr_reader :ui
   end
 end
-
-
-#
-# Set gem version
-#
-# module SensuYieldbotConsul
-#   # Gem version
-#   VERSION = '0.0.1'
-#   # NAME = 'Sensu-Yieldbot-Consul'
-#   # TOTAL = format(NAME, VERSION)
-#   # def json_version
-#   #       {
-#   #         'version' => TOTAL
-#   #       }.to_json
-#   #     end
-# end
---
+++ lib/sensu-yieldbot-consul/version.rb
@@ -0,0 +1,28 @@
+require 'json'
+
+# encoding: utf-8
+module SensuYieldbotConsul
+  # This defines the version of the gem
+  module Version
+    MAJOR = 0
+    MINOR = 0
+    PATCH = 4
+
+    STRING = [MAJOR, MINOR, PATCH].compact.join('.')
+
+    NAME   = 'Sensu-Yieldbot-Consul'
+    BANNER = "#{NAME} v%s"
+
+    module_function
+
+    def version
+      format(BANNER, STRING)
+    end
+
+    def json_version
+      {
+        'version' => STRING
+      }.to_json
+    end
+  end
+end
